2061c7d20c2a6e39526ee05882f17d0b6a419a19
Grigoli Tsitokhtsevi <tsitokhtsev2002@gmail.com>
2024-05-06 18:28:35 +0400

    style: refactored boilerplate and removed redundant files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import './assets/main.css'
+import { createApp } from 'vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue';
 
-createApp(App).mount('#app')
+import '@/assets/main.css';
+
+createApp(App).mount('#app');
